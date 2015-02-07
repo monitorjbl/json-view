561147510351ae66d422ab71300344a7ecaca963
Taylor Jones <monitorjbl@gmail.com>
2015-02-07 03:38:19 -0500

    fixing imports

--- src/main/java/com/monitorjbl/json/JsonWrapperSerializer.java
+++ src/main/java/com/monitorjbl/json/JsonWrapperSerializer.java
@@ -12,7 +12,6 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.Stack;
 
