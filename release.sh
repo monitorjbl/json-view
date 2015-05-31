b8bd1f8cb1813ff20a1ee22ee500699e8b7601f4
Taylor Jones <monitorjbl@gmail.com>
2015-05-31 17:55:00 -0400

    incrementing snapshot version

--- release.sh
+++ release.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 cd json-view
-mvn versions:set -DnewVersion=0.4 -DgenerateBackupPoms=false && cd ..
+mvn versions:set -DnewVersion=$1 -DgenerateBackupPoms=false && cd ..
 cd spring-json-view
-mvn versions:set -DnewVersion=0.4 -DgenerateBackupPoms=false && cd ..
+mvn versions:set -DnewVersion=$1 -DgenerateBackupPoms=false && cd ..
+
+if [[ $1 == *"-SNAPSHOT" ]]; then
+  git commit -a -m "incrementing snapshot version"
+else
+  git commit -a -m "$1 release"
+fi
