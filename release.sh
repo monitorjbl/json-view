7a09637956c67dd85389366542484df99439755a
Taylor Jones <monitorjbl@gmail.com>
2015-06-01 17:39:53 -0400

    0.5 release
    fixes #6
    
    fixes #7

--- release.sh
+++ release.sh
@@ -7,6 +7,4 @@ mvn versions:set -DnewVersion=$1 -DgenerateBackupPoms=false && cd ..
 
 if [[ $1 == *"-SNAPSHOT" ]]; then
   git commit -a -m "incrementing snapshot version"
-else
-  git commit -a -m "$1 release"
 fi
