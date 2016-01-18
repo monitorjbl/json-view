54e264d503fa91c02c2b2dfde0c6ecad886466be
Taylor Jones <monitorjbl@gmail.com>
2016-01-17 19:04:44 -0500

    documenting jackson configuration class

--- spring-json-view/src/test/java/com/monitorjbl/json/server/JacksonConfiguration.java
+++ spring-json-view/src/test/java/com/monitorjbl/json/server/JacksonConfiguration.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+/**
+ * Configures Jackson mapper for unit tests. Works with XML-based configuration
+ * if it is included as a bean.
+ */
 public class JacksonConfiguration {
   private final ObjectMapper mapper;
 
@@ -13,7 +17,7 @@ public class JacksonConfiguration {
     configureJackson(mapper);
   }
 
-  public static ObjectMapper configureJackson(ObjectMapper mapper){
+  public static ObjectMapper configureJackson(ObjectMapper mapper) {
     mapper.enable(SerializationFeature.INDENT_OUTPUT);
     mapper.setSerializationInclusion(Include.NON_NULL);
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
