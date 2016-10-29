228a0c9c03baece1c96c48b403411c723c7d6ecc
Taylor Jones <tayjones@cisco.com>
2016-10-29 19:01:38 -0400

    Randomizing port for test server

--- spring-json-view/src/test/java/com/monitorjbl/json/ConfigTest.java
+++ spring-json-view/src/test/java/com/monitorjbl/json/ConfigTest.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,13 +32,20 @@ public abstract class ConfigTest {
 
   protected static final Logger log = LoggerFactory.getLogger(XmlConfigurationTest.class);
   protected static ConfigServer server;
+  protected static int port = -1;
 
   public static void start() throws Exception {
-    server.start(8080);
+    //get random port
+    ServerSocket sock = new ServerSocket(0);
+    port = sock.getLocalPort();
+    sock.close();
+
+    //start server
+    server.start(port);
     boolean ready = false;
     while(!ready) {
       try {
-        new URL("http://localhost:8080/ready").openStream();
+        new URL("http://localhost:"+port+"/ready").openStream();
         ready = true;
       } catch(Exception e) {
         Thread.sleep(500);
@@ -48,7 +56,7 @@ public abstract class ConfigTest {
   @Test
   @SuppressWarnings("unchecked")
   public void testSimple() throws IOException {
-    Map<String, Object> map = new ObjectMapper().readValue(Request.Get("http://localhost:8080/bean").execute().returnContent().asStream(), HashMap.class);
+    Map<String, Object> map = new ObjectMapper().readValue(Request.Get("http://localhost:"+port+"/bean").execute().returnContent().asStream(), HashMap.class);
     assertEquals("ignored", map.get("ignoredDirect"));
     assertNull(map.get("int1"));
   }
@@ -56,7 +64,7 @@ public abstract class ConfigTest {
   @Test
   @SuppressWarnings("unchecked")
   public void testSimpleWithReturnValue() throws IOException {
-    Map<String, Object> map = new ObjectMapper().readValue(Request.Get("http://localhost:8080/bean/withReturnValue").execute().returnContent().asStream(),
+    Map<String, Object> map = new ObjectMapper().readValue(Request.Get("http://localhost:"+port+"/bean/withReturnValue").execute().returnContent().asStream(),
         HashMap.class);
     assertEquals("ignored", map.get("ignoredDirect"));
     assertNull(map.get("int1"));
@@ -65,7 +73,7 @@ public abstract class ConfigTest {
   @Test
   @SuppressWarnings("unchecked")
   public void testList() throws IOException {
-    List<Map<String, Object>> list = new ObjectMapper().readValue(Request.Get("http://localhost:8080/list").execute().returnContent().asStream(), ArrayList.class);
+    List<Map<String, Object>> list = new ObjectMapper().readValue(Request.Get("http://localhost:"+port+"/list").execute().returnContent().asStream(), ArrayList.class);
 
     assertEquals(2, list.size());
     for(Map<String, Object> map : list) {
@@ -120,7 +128,7 @@ public abstract class ConfigTest {
 
   @Test
   public void testNoninterference() throws Exception {
-    String ret = Request.Post("http://localhost:8080/bean").bodyString(
+    String ret = Request.Post("http://localhost:"+port+"/bean").bodyString(
         "{\"date\":\"1433214360187\",\"str1\":\"test\",\"notReal\":\"asdfas\"}", ContentType.APPLICATION_JSON)
         .execute().returnContent().asString();
     assertEquals(5, ret.split("\n").length);
@@ -129,7 +137,7 @@ public abstract class ConfigTest {
   @Test
   @SuppressWarnings("unchecked")
   public void testCircularDependency() throws Exception {
-    String ret = Request.Get("http://localhost:8080/circularReference").execute().returnContent().asString();
+    String ret = Request.Get("http://localhost:"+port+"/circularReference").execute().returnContent().asString();
     Map<String, Object> map = new ObjectMapper().readValue(ret, HashMap.class);
     assertNotNull(map.get("val"));
     assertEquals("parent", map.get("val"));
@@ -139,7 +147,7 @@ public abstract class ConfigTest {
   @Test
   @SuppressWarnings("unchecked")
   public void testResponseEntitySupport() throws Exception {
-    HttpResponse response = Request.Get("http://localhost:8080/responseEntity").execute().returnResponse();
+    HttpResponse response = Request.Get("http://localhost:"+port+"/responseEntity").execute().returnResponse();
     Map<String, Object> map = new ObjectMapper().readValue(response.getEntity().getContent(), HashMap.class);
 
     assertEquals(202, response.getStatusLine().getStatusCode());
@@ -152,7 +160,7 @@ public abstract class ConfigTest {
   @Test
   @SuppressWarnings("unchecked")
   public void testDefaultViewSupport() throws Exception {
-    HttpResponse response = Request.Get("http://localhost:8080/defaultView").execute().returnResponse();
+    HttpResponse response = Request.Get("http://localhost:"+port+"/defaultView").execute().returnResponse();
     Map<String, Object> map = new ObjectMapper().readValue(response.getEntity().getContent(), HashMap.class);
 
     assertEquals(4, map.get("id"));
@@ -163,7 +171,7 @@ public abstract class ConfigTest {
   @Test
   @SuppressWarnings("unchecked")
   public void testDefaultViewSupportWithInheritance() throws Exception {
-    HttpResponse response = Request.Get("http://localhost:8080/defaultViewInheritance").execute().returnResponse();
+    HttpResponse response = Request.Get("http://localhost:"+port+"/defaultViewInheritance").execute().returnResponse();
     Map<String, Object> map = new ObjectMapper().readValue(response.getEntity().getContent(), HashMap.class);
 
     assertEquals(4, map.get("id"));
@@ -175,7 +183,7 @@ public abstract class ConfigTest {
   @Test
   @SuppressWarnings("unchecked")
   public void testDefaultViewSupportWithLists() throws Exception {
-    HttpResponse response = Request.Get("http://localhost:8080/defaultViewList").execute().returnResponse();
+    HttpResponse response = Request.Get("http://localhost:"+port+"/defaultViewList").execute().returnResponse();
     List<Map<String, Object>> list = new ObjectMapper().readValue(response.getEntity().getContent(), ArrayList.class);
 
     assertEquals(1, list.size());
@@ -188,7 +196,7 @@ public abstract class ConfigTest {
   @Test
   @SuppressWarnings("unchecked")
   public void testDefaultViewSupportWithSets() throws Exception {
-    HttpResponse response = Request.Get("http://localhost:8080/defaultViewSet").execute().returnResponse();
+    HttpResponse response = Request.Get("http://localhost:"+port+"/defaultViewSet").execute().returnResponse();
     Set<Map<String, Object>> set = new ObjectMapper().readValue(response.getEntity().getContent(), HashSet.class);
 
     assertEquals(1, set.size());
@@ -201,7 +209,7 @@ public abstract class ConfigTest {
   @Test
   @SuppressWarnings("unchecked")
   public void testDefaultViewSupportWithMaps() throws Exception {
-    HttpResponse response = Request.Get("http://localhost:8080/defaultViewMap").execute().returnResponse();
+    HttpResponse response = Request.Get("http://localhost:"+port+"/defaultViewMap").execute().returnResponse();
     Map<String, Map<String, Object>> map = new ObjectMapper().readValue(response.getEntity().getContent(), HashMap.class);
 
     assertNotNull(map.get("myobj"));
